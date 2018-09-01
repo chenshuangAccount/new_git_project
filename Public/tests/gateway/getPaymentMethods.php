b00e78e5ba8b86757fc098bc828668fadb81578d
AdminAccount <2390997621@qq.com>
2018-09-01 21:36:25 +0800

    bdsjdfsdf

--- Public/tests/gateway/getPaymentMethods.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$client = new Yar_Client('http://dev.sll.payment.daojia.com.cn/payment/gateway');
-$params['city_id'] = '1';
-$params['platform_logic'] = '31';
-$result = $client->getPaymentMethods($params);
-var_dump($result);
